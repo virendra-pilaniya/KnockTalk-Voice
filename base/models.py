9dc2783c83a0de07409fd85e99d60baa1ee64db9
virendra-pilaniya <pilaniyavirendra@gmail.com>
2024-01-02 01:36:06 +0530

    css improved

--- base/models.py
+++ base/models.py
@@ -12,6 +12,8 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete = models.SET_NULL, null = True)
     name = models.CharField(max_length = 50)
     description = models.TextField(null = True, blank = True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     updated_at = models.DateTimeField(auto_now = True)
     created_at = models.DateTimeField(auto_now_add = True)
     
--- base/views.py
+++ base/views.py
@@ -70,6 +70,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_message = room.message_set.all().order_by('-created_at')
+    participant = room.participants.all()
     
     if request.method == 'POST':
         message = Message.objects.create(
@@ -77,9 +78,10 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
     
-    context = {'room' : room, 'room_message' : room_message}     
+    context = {'room' : room, 'room_message' : room_message, 'participants' : participant}     
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
